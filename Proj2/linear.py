c057c7eb6bfc2594b19ca5eee3a64ab3eef9ebfd
eg10yx <34946305+eg10yx@users.noreply.github.com>
2020-05-10 17:58:09 +0200

    some module

---
+++ Proj2/linear.py
@@ -0,0 +1,61 @@
+import torch
+from torch import empty
+from module import Module
+
+torch.set_grad_enabled(False)
+
+class Linear(Module):
+    def __init__(self, input_size, hidden_layers):
+        
+        self.input_size = input_size
+        self.hidden_layer_size = hidden_layers
+        self.input = empty(input_size)
+        self.output = empty(hidden_layers)
+
+        self.weights = empty(hidden_layers, input_size).uniform_(-1, 1)
+        self.biases = empty(hidden_layers).uniform_(-1, 1)
+
+        self.weights_gradients = [empty(hidden_layers, input_size).zero_()]
+        self.biases_gradients = [empty(hidden_layers).zero_()]
+
+
+    def forward(self, input_tensor):
+
+        self.input = input_tensor
+        self.output = self.weights @ input_tensor + self.biases
+        return self.output
+
+    def backward(self, grad_output, momentum=None):
+        
+        grad_input = self.weights.transpose(0, 1) @ grad_output
+        biases_gradients = grad_output
+        weights_gradients = grad_output.view(-1, 1) @ self.input.view(1, -1)
+
+        if momentum is None:
+            self.biases_gradients.append(biases_gradients)
+            self.weights_gradients.append(weights_gradients)
+        else:
+            self.biases_gradients.append(momentum * self.biases_gradients[-1] + (1-momentum) * biases_gradients)
+            self.weights_gradients.append(momentum * self.weights_gradients[-1] + (1-momentum) * weights_gradients)
+
+        return grad_input
+
+    def gradient_step(self, step_size):
+
+        self.weights -= step_size * self.weights_gradients[-1]
+        self.biases -= step_size * self.biases_gradients[-1]
+
+    def param(self):
+
+        return [(self.weights[i, :], self.weights_gradients[i, :]) for i in range(self.hidden_layer_size)] + [(self.biases, self.biases_gradients)]
+
+    def get_hidden_layer_size(self):
+
+        return self.hidden_layer_size
+
+    def get_input_size(self):
+
+        return self.input_size
+
+    def summary(self):
+        print('\tFully connected layer of {} hidden units'.format(self.hidden_layer_size))
---
+++ Proj2/relu.py
@@ -0,0 +1,46 @@
+import torch
+from torch import empty
+from module import Module
+
+torch.set_grad_enabled(False)
+
+class ReLU(Module):
+
+    def __init__(self, input_size):
+
+        self.hidden_layer_size = input_size
+        self.input = empty(input_size)
+        self.output = empty(input_size)
+        self.grad_wrt_input = empty(input_size)
+
+    def forward(self, input_tensor):
+
+        self.output = input_tensor
+        self.output[input_tensor < 0] = 0
+        return self.output
+
+    def backward(self, grad_output, step_size=None, momentum=None):
+
+        derivative = self.output
+        derivative[derivative != 0] = 1
+        self.grad_input = grad_output * derivative
+        return self.grad_input
+
+    def gradient_step(self, step_size=None):
+
+        pass
+
+    def param(self):
+
+        return []
+
+    def get_hidden_layer_size(self):
+
+        return self.hidden_layer_size
+
+    def get_input_size(self):
+
+        return self.hidden_layer_size
+
+    def summary(self):
+        print('\tReLU activation layer of size {}'.format(self.hidden_layer_size))
